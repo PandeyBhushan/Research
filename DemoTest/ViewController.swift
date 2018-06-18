1ff01ca26ab2c92e5f545a1a530af04a7bbebc50
Chandra Bhushan Pandey <chandra.bhushan.pandey@excelityglobal.com>
2018-06-18 17:43:29 +0530

    checking poll

--- DemoTest/ViewController.swift
+++ DemoTest/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("test")
         print("another message for schedular")
+        print("checking poll")
     }
 
     override func didReceiveMemoryWarning() {
