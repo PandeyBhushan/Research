147d4aa6c6d1dea8733d65acef365c5503b100ec
Chandra Bhushan Pandey <chandra.bhushan.pandey@excelityglobal.com>
2018-06-18 18:09:19 +0530

    committing and build trigger

--- DemoTest/ViewController.swift
+++ DemoTest/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         print("test")
         print("another message for schedular")
         print("checking poll")
+        print("new build viw comminting the code")
         
     }
 
